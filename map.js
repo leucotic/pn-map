e074ce1ddcfe35bdd18bd29704a1d0330c104ebd
Masha <leucotic@users.noreply.github.com>
2020-07-28 11:09:50 -0400

    Update map.js

--- map.js
+++ map.js
@@ -12,7 +12,7 @@ var maptiles = new L.StamenTileLayer("watercolor", {
                 'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
                 'Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, ',
                 'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
-            ].join("")
+            ].join("");
 });
 
 var labels = new L.StamenTileLayer("toner-labels");
